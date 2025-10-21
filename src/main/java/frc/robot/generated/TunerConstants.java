4402858650c30fda0a90385cebca75c2a813206c
Jay <44783760+jschenke488@users.noreply.github.com>
2025-10-20 19:42:57 -0500

    feat: Add temporary speed multiplier for kSpeedAt12Volts tuning

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -78,7 +78,9 @@ public class TunerConstants {
 
   // Theoretical free speed (m/s) at 12 V applied output;
   // This needs to be tuned to your individual robot
-  public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(5.41);
+  private static final double speedMultiplierTempThisShouldBeRemoved = 2.0;
+  public static final LinearVelocity kSpeedAt12Volts =
+      MetersPerSecond.of(5.41 * speedMultiplierTempThisShouldBeRemoved);
 
   // Every 1 rotation of the azimuth results in kCoupleRatio drive motor turns;
   // This may need to be tuned to your individual robot
